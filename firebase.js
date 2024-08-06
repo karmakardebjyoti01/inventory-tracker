7fb8330bd0f04d991c7579220fb62c2b458f4e70
karmakardebjyoti01 <debjyotiprofessional.net02@gmail.com>
2024-08-06 10:54:57 +0530

    Add Firebase config using environment variables

--- firebase.js
+++ firebase.js
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAF8cpmVSG-r7s3dqNBZZImzoFYMvqrEJU",
-  authDomain: "inventory-tracker-c95fd.firebaseapp.com",
-  projectId: "inventory-tracker-c95fd",
-  storageBucket: "inventory-tracker-c95fd.appspot.com",
-  messagingSenderId: "594582864900",
-  appId: "1:594582864900:web:1fec59fb55394df501e151",
-  measurementId: "G-5Q37FP2GK7"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
